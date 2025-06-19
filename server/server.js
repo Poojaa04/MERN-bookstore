7670657cef55fa2c8386fdeb2312e0a6ac96835d
POOJA <poojabhat2004@gmail.com>
2025-06-19 20:25:32 +0530

    added mongo atlas

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/book-store');
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
 app.use(cors()); // Use the cors middleware
